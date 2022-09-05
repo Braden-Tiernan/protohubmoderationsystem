8cf2555462e18fadbb9cdb6346abe03731d56ef3
Braden Tiernan <112308003+Braden-Tiernan@users.noreply.github.com>
2022-09-05 10:03:26 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<!--Programmed By Braden Tiernan-->
+<!--
+font-family: 'Open Sans', sans-serif;
+font-family: 'Roboto', sans-serif;
+-->
+<html lang="en">
+  <script>
+    var lv1 = 0;
+    var lv2 = 0;
+  </script>
+  <head>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-1C6EFZECX3"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-1C6EFZECX3');
+    </script>
+    <script type='text/javascript'>
+      window.smartlook||(function(d) {
+      var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+      var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+      c.charset='utf-8';c.src='https://rec.smartlook.com/recorder.js';h.appendChild(c);
+      })(document);
+      smartlook('init', '915da08d39a5e3eb64103e4b04afc1723d8b499c');
+    </script>
+    <title>Moderation | ProtoHub</title>
+    <link type="x-image/icon" rel="shortcut icon" href="//protohub.online/ProtoHub/ProtoHub%20Logo%20Vector.svg">
+    <link onLoad="lv1 = lv1 + 1;" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet">
+    <link onLoad="lv1 = lv1 + 1;" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;900&display=swap" rel="stylesheet">
+    <link onLoad="lv1 = lv1 + 1;" id="pagestyle" rel="stylesheet" type="text/css" href="//protohub.online/lightss.php">
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@PawsCyro" />
+    <meta name="twitter:creator" content="@PawsCyro" />
+    <meta name="twitter:image" content="//protohub.online/ProtoHub/ProtoHub%20LogoText.png" />
+    <meta property="og:image" content="//protohub.online/ProtoHub/ProtoHub%20LogoText.png">
+    <meta property="og:image:alt" content="ProtoHub Logo">
+    <meta name="twitter:description" content="ProtoHub, the Best place for Protogen Art & Furry Content. In my opinion." />
+    <meta property="og:title" content="Moderation | ProtoHub" />
+    <meta property="og:description" content="ProtoHub, the Best place for Protogen Art & Furry Content. In my opinion." />
+    <meta name="description" content="ProtoHub, the Best Place for Protogen art & furry content. In my opinion." />
+    <meta property="twitter:title" content="ProtoHub, the Best place for Protogen Art & Furry Content. In my opinion." />
+    <meta name="keywords" content="ProtoHub, ProtoHub Happy Art, ProtoHub Furry Happy Art, ProtoHub Protogen Happy Art, ProtoHub Synth Happy Art, Cute Synth Furry Happy Art, Cute Protogen Furry Happy Art, Protogen Furry Happy Art, Protogen Happy, Furries Happy" />
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="ProtoHub"/>
+    <meta property="og:url" content="http://protohub.online/" />
+    <meta name="DC.title" content="Moderation | ProtoHub" />
+    <meta name="viewport" content="width=device-width, initial-scale=0.9">
+  </head>
+  <body onscroll="f3()" style="background-image:none !important;">
+    <div class="seotext"><h1>ProtoHub the furry protogen art website!</h1><p>This is <strong>ProtoHub</strong>, the absolute best place for <strong>protogen happy art</strong>, <strong>synth art</strong>, and <strong>G.E.M. art</strong>!!<br><strong>ProtoHub</strong> is an image posting website for the furry community, with the pourposte of helping <strong>mehcanical</strong> / <strong>electrical</strong> <strong>furries</strong> share art and to help the <strong>furry community</strong> find and grow that side of the <strong>community</strong>.</p></div>
+    <div id="loadingscreen" style=""><div class="s1"><h1 class="s1s9">Loading..</h1><p class="s1s8">Let's get posts checked!</p></div></div>
+    <div class="sep1"></div>
+    <div class="cookiess1" id="cookiealertbar"><span class="cookiess2">Our Protogens need cookies.<br>By using this site you agree to cookies and our <a href="//protohub.online/legal.php" target="_blank" class="txtlink">Terms Of Service</a>.<br><a onclick="document.getElementById('cookiealertbar').remove();setCookie('v1', 'allow', 7);" class="cookiess3 s1s6s1sc3">Alright</a><a href="//protohub.online/legal.php" target="_blank" class="txtlink">Learn More</a></span></div>
+    <div class="s1 s1sc3">
+      <h1 class="s1s9">Let's Moderate!</h1>
+      <p class="s1s8"><!--Only you have the power to make the world a better/safer place.<br>-->"What you do makes a difference, and you have to decide what kind of difference you want to make" - Jane Goodall<!--<br>"Moderation is the secret to survival" - Manly Hall<br>"Moderation in all things - including moderation" - Benjamin Franklin--></p>
+      <h1 class="s1s5">Your tasks. <span class="bluetext">(3)</span></h1>
+      <div class="sep3"></div>
+    </div>
+    <div class="s1 s1sc2 s1sc3">
+      <a onClick="f2()" href="//protohub.online/modpost/?postid=30"><div class="s1s4p2"><div class="s1s4s1p2"><img class="s1s4s1s1p2" src="//protohub.online/getimage2/?id=30"></div><div class="s1s4s2"><a class="s1s4s2s1"><strong>Post</strong></a></div></div></a>
+      <a onClick="f2()" href="//protohub.online/modcomm/?postid=30"><div class="s1s4p2"><div class="s1s4s1p2"><img class="s1s4s1s1p2" src="//protohub.online/getimage2/?id=30"></div><div class="s1s4s2"><a class="s1s4s2s1"><strong>Comment(s)</strong></a></div></div></a>
+      <a onClick="f2()" href="//protohub.online/modacc/?postid=CyroPaws"><div class="s1s4p2"><div class="s1s4s1p2"><img class="s1s4s1s1p2" src="//protohub.online/getuser/?id=CyroPaws"></div><div class="s1s4s2"><a class="s1s4s2s1"><strong>Account</strong></a></div></div></a>
+    </div>
+    <div class="s1 s1sc3">
+      <p class="s1s8"><strong>Tap on the tasks above and answer the questions to complete your mod tasks!</strong></p>
+      <div class="sep1"></div>
+      <p class="s1s8">__________</p>
+      <h1 class="s1s9">Your Info.</h1>
+      <h1 class="s1s5">Signed in as <span class="bluetext"></span>.</h1>
+      <h1 class="s1s5">You have a mod score of <span class="bluetext">2500</span>.</h1>
+      <h1 class="s1s5">You have <span class="redtext">2</span> mod strikes!</h1>
+      <p class="s1s8sc1 s1s8sc2 ">&bull; Once your tasks are complete, you're done!!<br>&bull; Check this page <strong>once a day</strong> for new tasks.<br>&bull; If you miss one or more tasks without reporting to the admin beforehand you'll get a strike! D:<br>&bull; Always contact the admin if you'll be too busy to moderate beforehand.<br>&bull; <strong>Five strikes and you're out!</strong><br><br><a target="_blank" href="https://t.me/CyroPaws" class="txtlink">Contact Admin Via Telegram</a><br><a target="_blank" href="https://twitter.com/PawsCyro" class="txtlink">Contact Admin Via Twitter</a><br><a target="_blank" href="mailto:cyro.paws@gmail.com" class="txtlink">Contact Admin Via E-mail</a></p>
+      <div class="sep2"></div>
+    </div>
+    <div class="topbars1">
+      <div class="topbars2">
+        <div class="topbars3">
+          <img onClick="openmenu()" class="topbars4" src="//protohub.online/generated (2).svg">
+          <span class="topbars5"><a onClick="f2();" href="//protohub.online" class="topbars5s1">Home</a><a onClick="f2();" href="//protohub.online/search/?v1=no" class="topbars5s1">Search</a><a onClick="f2();" href="//protohub.online/CreateAccount.php" class="topbars5s1">Account</a><a onClick="f2();" href="//protohub.online/feedback/" class="topbars5s1">Feedback</a><a onClick="f2();" href="//protohub.online/advertising" class="topbars5s1">Advertising</a><a onClick="SetSignOut();" class="topbars5s2">sign in/out</a><a onClick="lightmodechange();" class="topbars5s2">switch theme</a></span>
+        </div>
+      </div>
+    </div>
+    <div id="s2" class="s2"></div>
+  </body>
+  <script>
+    window.onpageshow = function(event) {
+      if (event.persisted) {
+        document.getElementById("loadingscreen").style.pointerEvents = "none";
+        document.getElementById("loadingscreen").style.opacity = 0;
+      }
+    }
+    
+    var lightmodechangev1 = true;
+    var openmenuv1 = false;
+    var f1f2v1 = true;
+
+    setTimeout(f1, 1700);
+    function f1() {
+      if (lv1 >= lv2) {
+        document.getElementById("loadingscreen").style.pointerEvents = "none";
+        document.getElementById("loadingscreen").style.opacity = 0;
+        setTimeout(f1f2, 1000);
+      } else {
+        setTimeout(f1, 100);
+      }
+    }
+    function f1f2() {
+      if ((window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) && f1f2v1) {
+        lightmodechange();
+      }
+    }
+
+    function f2() {
+      document.getElementById("loadingscreen").style.pointerEvents = "auto";
+      document.getElementById("loadingscreen").style.opacity = 1;
+      setTimeout(f1, 3000);
+    }
+    
+    function swapStyleSheet(sheet) {
+      document.getElementById("pagestyle").setAttribute("href", sheet);  
+    }
+    
+    function lightmodechange() {
+      if (lightmodechangev1) {
+        document.getElementById("loadingscreen").style.backgroundColor = "black";
+        swapStyleSheet("//protohub.online/darkss.php)";
+        setCookie("v2", "black", 7);
+      } else {
+        document.getElementById("loadingscreen").style.backgroundColor = "white";
+        swapStyleSheet("//protohub.online/lightss.php");
+        setCookie("v2", "white", 7);
+      }
+      lightmodechangev1 = !lightmodechangev1;
+    }
+    
+    function setCookie(cname, cvalue, exdays) {
+      var d = new Date();
+      d.setTime(d.getTime() + (exdays*24*60*60*1000));
+      var expires = "expires="+ d.toUTCString();
+      document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+
+    function getCookie(cname) {
+      var name = cname + "=";
+      var decodedCookie = decodeURIComponent(document.cookie);
+      var ca = decodedCookie.split(';');
+      for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+        }
+      }
+      return "";
+    }
+
+    function SetSignOut() {
+      if (getCookie("v3") == "") {
+        f2();
+        window.location = "//protohub.online/AccountSignin.php";
+      } else {
+        setCookie("v3", "", -7);
+        setCookie("v4", "", -7);
+        f2();
+        location.reload();
+      }
+    }
+    
+    function openmenu() {
+      openmenuv1 = !openmenuv1;
+      if (openmenuv1) {
+        document.getElementById("s2").style.opacity = 1;
+        document.getElementById("s2").style.pointerEvents = "auto";
+        document.body.style.overflow = "hidden";
+        document.getElementById("s2").innerHTML = `<div class="s2s1">
+        <p class="s2s1s1">The Menu.</p>
+        <a class="s2s1s2sc1" onClick="f2();" href="//protohub.online"><p class="s2s1s2">Home</p></a>
+        <a class="s2s1s2sc1" onClick="f2();" href="//protohub.online/search/?v1=no"><p class="s2s1s2">Search</p></a>
+        <a class="s2s1s2sc1" onClick="f2();" href="//protohub.online/CreateAccount.php"><p class="s2s1s2">Account</p></a>
+        <a class="s2s1s2sc1" onClick="f2();" href="//protohub.online/feedback/"><p class="s2s1s2">Feedback</p></a>
+        <a class="s2s1s2sc1" onClick="f2();" href="//protohub.online/advertising"><p class="s2s1s2">Advertising</p></a>
+        <a class="s2s1s2sc1" target="_blank" href="https://www.patreon.com/protohub"><p class="s2s1s2">The Patreon!!</p></a>
+        <div class="sep3"></div>
+        <p class="s2s1s4" onClick="openmenu();">Exit</p>
+        <p class="s2s1s3" onClick="SetSignOut();">Sign In/Out</p>
+        <p class="s2s1s3" onClick="lightmodechange();openmenu();">Switch Theme</p>
+        <div class="sep1"></div>
+      </div>`;
+      } else {
+        document.getElementById("s2").style.opacity = 0;
+        document.getElementById("s2").style.pointerEvents = "none";
+        document.body.style.overflow = "scroll";
+        document.getElementById("s2").innerHTML = ``;
+      }
+      if (document.getElementById("topbars7")) {document.getElementById("topbars7").remove();}
+    }
+  </script>
+</html>
